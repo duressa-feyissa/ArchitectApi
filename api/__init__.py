100a70973e641d04fd591f03c91b3267e53b9154
duressa-feyissa <duresafeyisa2022@gmail.com>
2023-09-03 09:09:29 +0300

    api

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,24 @@ from .routes.user import router as user_router
 from .routes.home import router as home_router
 from .common import models
 from .common.database import engine
+from fastapi.middleware.cors import CORSMiddleware
+import os
+PORT = os.getenv("PORT", 8000)
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    f"http://localhost:{PORT}",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 models.Base.metadata.create_all(bind=engine)
 
