d3ad1da0e0c225ee772a7424204c8d47ff5eb4b5
duressa-feyissa <duresafeyisa2022@gmail.com>
2023-09-03 09:22:01 +0300

    api

--- api/common/image.py
+++ api/common/image.py
@@ -1,14 +1,4 @@
 import cloudinary
 from cloudinary.uploader import upload
           
-cloudinary.config( 
-  cloud_name = "dtghsmx0s", 
-  api_key = "155646927271619", 
-  api_secret = "kYyrS0ssz2NlVjQw0i17Z5ZnnfY" 
-)
 
-# Upload an image
-response = upload("example.jpg")
-
-# Print the URL of the uploaded image
-print("Uploaded image URL:", response['secure_url'])
